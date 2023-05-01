1662d8b8e873d3becf90a2e294e3fb6ab95aae27
Pruthvisinh Rajput <pruthvirajput97@gmail.com>
2023-05-01 15:56:44 +0530

    deploy in github

---
+++ build/precache-manifest.8151de1385191943218116204f09b8f3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e33038e38675129ab4842c46f1ec1014",
+    "url": "/ToDoList_v2/index.html"
+  },
+  {
+    "revision": "7441b963c4cd19361e97",
+    "url": "/ToDoList_v2/static/js/2.71074f19.chunk.js"
+  },
+  {
+    "revision": "665e0bcb70cf1a99c5dc",
+    "url": "/ToDoList_v2/static/js/main.661cbe95.chunk.js"
+  },
+  {
+    "revision": "dc41f450b39c2eee9129",
+    "url": "/ToDoList_v2/static/js/runtime-main.499eaf74.js"
+  }
+]);
